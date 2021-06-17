d09ec7ba23f4d0bfa66b27a8f12ad552de1ed6c4
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-06-17 18:47:01 +0200

    Sufficiency index

--- utils/fairness_measures.py
+++ utils/fairness_measures.py
@@ -100,3 +100,54 @@ def get_separation(df, target_col, prediction_col, positive_value, cols):
 
     return dic_separation
 
+
+def get_sufficiency(df, target_col, prediction_col, positive_value, cols):
+    """
+    Args:
+        df (DataFrame):Input data
+        target_col (str): ground truth of what we want to predict
+        prediction_col (str): name of the column containing the prediction
+        positive_value (str): positive outcome for the binary classification task
+        cols (list): List of features that need to be inspected
+
+    Returns:
+         dictionaries whose the key are the columns and whose values are
+         the calculated probabilities within each class. One dictionary
+         for the positive class, and one for the negative one
+
+         P( Y = 1 | R = 1, A = a ) = P( Y = 1 | R = 1, A = b )
+            * at most with a small tolerance
+            * R = target
+            * Y = predictor
+
+    """
+    dic_sufficiency = defaultdict(list)
+
+    # get the values from the target
+    target_classes = df[target_col].unique()
+
+    print()
+
+    for col in cols:
+
+        # the outcome is fixed and it is based on the predictor
+
+        # get the classes within the current column
+        classes = df[col].unique()
+
+        for c in classes:
+            # numerator = P ( Y = 1 ) \intersection P ( R = 1 ) * P ( A = a) )
+            # denom = P ( R = 1 ) * P ( A = a)
+
+            # for both numerator and denominator I am not considering the division by len(df)
+
+            for target in target_classes:
+                numerator = len(df[(df[prediction_col] == positive_value) & (df[target_col] == target) & (df[col] == c)])
+                denom = len(df[(df[target_col] == target) & (df[col] == c)])
+
+                # set two different list for each col, one for each class (0/1)
+                key = col + "-" + str(target)
+                dic_sufficiency[key].append(numerator / denom)
+
+    return dic_sufficiency
+

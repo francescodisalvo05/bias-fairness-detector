273694fcad0cfc80bdf284f91b729d9c725a538d
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-06-18 21:13:15 +0200

    Test fairness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils.bias_measures import *
 from utils.fairness_measures import *
+from utils.model import *
 
 
 def bias(df_main, sensitive_list):
@@ -9,22 +10,17 @@ def bias(df_main, sensitive_list):
     print("Imbalanced :  ", get_imbalance_ratio(df_main,  sensitive_list))
 
 
-def fairness(df_main, prediction_col, positive_outcome, sensitive_list):
-    # to do : ML model for the prediction over Two_yr_Recidivism
-    # it should be the "predictor"
-    print("Indipendence :  ", get_indipendence(df_main, prediction_col, sensitive_list))
+def fairness(df_main, target_col, prediction_col, positive_outcome, sensitive_list):
 
-    # >> it will be both one because predictor and target are the same
-    print("Separation :  ", get_separation(df_main, prediction_col, prediction_col, positive_outcome, sensitive_list))
-    # >> it will be both one because predictor and target are the same
-    print("Sufficiency :  ", get_sufficiency(df_main, prediction_col, prediction_col, positive_outcome, sensitive_list))
+    print("Indipendence :  ", get_indipendence(df_main, prediction_col, sensitive_list))
+    print("Separation :  ", get_separation(df_main, target_col, prediction_col, positive_outcome, sensitive_list))
+    print("Sufficiency :  ", get_sufficiency(df_main, target_col, prediction_col, positive_outcome, sensitive_list))
 
 
 if __name__ == '__main__':
 
-    df = pd.read_csv('data/propublica_data_for_fairml_cleaned.csv.csv')
+    df = pd.read_csv('data/propublica_data_for_fairml_cleaned.csv')
+    df_with_prediction = get_prediction(df.drop(columns=['race']), 'score_factor')
 
-
-    # to do : update the DF in order to have just one "race" column instead of
-    # sensitive_cols = ['Female', 'Age_Above_FourtyFive', 'Age_Below_TwentyFive']
-    # fairness(df, 'Two_yr_Recidivism', 1, ['Female'])
+    # sensitive_cols = ['Female', 'Age_Above_FourtyFive', 'Age_Below_TwentyFive','African_American','Asian','Hispanic','Native_American','Other']
+    fairness(df_with_prediction, 'score_factor', 'prediction', 1, ['Asian'])

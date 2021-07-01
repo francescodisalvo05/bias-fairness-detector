f300d5378365668d904ac4db5f8ee137babf8998
francescodisalvo05 <francesco.disalvo05@community.unipa.it>
2021-07-01 22:50:03 +0200

    add default value on grouping method

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,27 +1,33 @@
 
 import pandas as pd
 
-def compact_binary_features(df, features, new_feature, new_path):
+
+def compact_binary_features(dir_dataset, features, new_feature, new_path, neutral=None):
     """
     Args:
          dir_dataset (str) : path of the dataset
          features (list) : list of binary features to merge
          new_feature (str) : name of the new categorical feature
          new_path (str) : directory + filename (e.g. 'data/cleaned_dataset.csv')
+         neutral (str) : str for a neutral combination (if all "non" positive have a meaning)
     Returns:
          Save the compacted dataset, for which we have a new column called
          "race", in which we add the race, given by the binary features
     """
-    df = pd.read_csv(df)
+    df = pd.read_csv(dir_dataset)
     new_feature_list = []
 
     # iterrows is very expansive
     # to do : try to use some more efficient workaround
     for index, row in df.iterrows():
+        found = False
         for feature in features:
             if row[feature] == 1:
                 new_feature_list.append(feature)
-                break
+                found = True
+
+        if not found:
+            new_feature_list.append(neutral)
 
     for feature in features:
         df.drop(columns=[feature], inplace=True)
